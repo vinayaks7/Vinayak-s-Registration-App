88ae62c07a3b23383ed24f427acc6faa85a5d15d
Vinayak.S <vinayakshivakumar7@gmail.com>
2021-08-01 10:26:30 +0530

    First Commit

---
+++ Android/VinayaksApp/app/src/main/java/com/example/vinayaksapp/MainActivity.java
@@ -0,0 +1,290 @@
+package com.example.vinayaksapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_VIDEO = 101;
+    private EditText name;
+    private EditText email;
+    private EditText phNo;
+    private EditText age;
+    private EditText gender;
+    private Uri imageUri;
+    private Uri videoUri;
+    private FirebaseStorage storage;
+    private FirebaseDatabase database;
+    private String TAG = "fafa";
+    private String videoUrltofirebase;
+    private String imageUrltofirebase;
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
+    ;
+
+
+    Uri picUri;
+
+    private static final int REQUEST_CAPTURE_VIDEO = 100;
+    private static final int PICK_IMAGE_REQUEST = 101;
+    private Uri filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        phNo = findViewById(R.id.phno);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        Button onSubmit = findViewById(R.id.onSubmit);
+        Button addPhoto = findViewById(R.id.addphoto);
+        Button addVideo = findViewById(R.id.addvideo);
+        Button shootVideo = findViewById(R.id.shootvideo);
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermission();
+
+
+            }
+        });
+
+        shootVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capturevideo();
+            }
+        });
+
+        addVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takerecordedVideo();
+            }
+        });
+
+    }
+
+    private void askCameraPermission() {
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else {
+
+            dispatchTakePictureIntent();
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                dispatchTakePictureIntent();
+            }else {
+                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+    private void capturevideo() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivity(intent);
+    }
+
+    private void takerecordedVideo() {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                REQUEST_CAPTURE_VIDEO);
+    }
+
+
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        Log.d(TAG,"Inside currentPath");
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        File storageDir=getExternalCacheDir();
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                Toast.makeText(this, "Reaching here100", Toast.LENGTH_SHORT).show();
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this, "Reaching here", Toast.LENGTH_SHORT).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.harry.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CAPTURE_VIDEO) {
+
+            if (data != null) {
+                if (data.getData() != null) {
+
+                    videoUri = data.getData();
+
+                    if (videoUri != null) {
+                        StorageReference reference = storage.getReference().child("Videos");
+                        reference.putFile(videoUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            videoUrltofirebase = uri.toString();
+
+                                            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Task failed", Toast.LENGTH_SHORT).show();
+                                }
+
+
+                            }
+
+                        });
+                    } else {
+                        Toast.makeText(this, "FilePath is null", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+            }
+
+
+        }
+        else if(requestCode==CAMERA_REQUEST_CODE){
+            if(resultCode== Activity.RESULT_OK){
+                File f=new File(currentPhotoPath);
+                picUri=Uri.fromFile(f);
+                Log.d(TAG,Uri.fromFile(f).toString());
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+                Calendar calendar=Calendar.getInstance();
+
+                if(contentUri!=null){
+                    StorageReference reference=storage.getReference().child("Images").child(calendar.getTimeInMillis()+"");
+                    reference.putFile(contentUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+//                                    imageUrl=uri.toString();
+//                                    database.getReference().child("Images").push().setValue(imageUrl);
+                                        Log.d("fafa",uri.toString());
+                                        Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Task failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(this, "FilePath is null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Android/VinayaksApp/app/src/main/java/com/example/vinayaksapp/User.java
@@ -0,0 +1,79 @@
+package com.example.vinayaksapp;
+
+public class User {
+
+    String name;
+    String phoneNo;
+    String emailId;
+    String age;
+    String gender;
+    String imageUri;
+    String videoUri;
+
+
+    public User(String name, String phoneNo, String emailid, String age, String gender, String imageUri, String videoUri) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.emailId = emailid;
+        this.age = age;
+        this.gender = gender;
+        this.imageUri = imageUri;
+        this.videoUri = videoUri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getVideoUri() {
+        return videoUri;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+}
